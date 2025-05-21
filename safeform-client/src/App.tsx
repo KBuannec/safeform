935ec1732b3526f7ea884b8d507a1cf6d921fad5
Kévin BUANNEC <kevin.buannec@proton.me>
2025-05-21 16:44:08 +0200

    feat: Barre de navigation

--- safeform-client/src/App.tsx
+++ safeform-client/src/App.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import LoginFormContainer from './LoginForm/LoginForm.container';
 import RegisterFormContainer from './RegisterForm/RegisterForm.container';
 import SafeFormContainer from './SafeForm/SafeForm.container';
 
 function App() {
+  const [view, setView] = useState<'register' | 'login' | 'safeform'>('register');
+
   return (
     <div className='app-container'>
-      {/* <SafeFormContainer /> */}
-      {/* <RegisterFormContainer /> */}
-      <LoginFormContainer />
+      <nav className="navigation">
+        <button onClick={() => setView('register')}>Inscription</button>
+        <button onClick={() => setView('login')}>Connexion</button>
+        <button onClick={() => setView('safeform')}>SafeForm</button>
+      </nav>
+
+      {view === 'register' && <RegisterFormContainer />}
+      {view === 'login' && <LoginFormContainer />}
+      {view === 'safeform' && <SafeFormContainer />}
+
     </div>
   );
 }
